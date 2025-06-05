ef10405dfb383506fc06c8d5fc3e5e2951bc3019
Oscar Tsai <chunyung8487@gmail.com>
2025-06-05 10:29:36 +0800

    2025-06-05 hero servicelist 加上router-dom

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Hero.css';
 import heroImage from '../assets/images/hero-acoustic-guitar.jpg'; // 引入圖片
+import { Link } from 'react-router-dom';
 
 function Hero() {
     return (
